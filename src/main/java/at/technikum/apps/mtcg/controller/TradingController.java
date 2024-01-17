1fa9b33d4c7c29bc05702754616e0c3f9de2a025
eberl <if23b057@technikum-wien.at>
2024-01-17 11:38:58 +0100

    Trade Beginning

---
+++ src/main/java/at/technikum/apps/mtcg/controller/TradingController.java
@@ -0,0 +1,10 @@
+package at.technikum.apps.mtcg.controller;
+
+import at.technikum.server.http.HttpStatus;
+import at.technikum.server.http.Request;
+import at.technikum.server.http.Response;
+
+public class TradingController{
+
+
+}
